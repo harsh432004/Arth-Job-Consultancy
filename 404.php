e4d239b310ff9a12ebfa5e2780d683ee23bd9d88
Harsh  Vaidya <harshvaidya432004@gmail.com>
2023-08-11 19:37:11 +0530

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,64 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Amwal 404</title>
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700&subset=devanagari,latin-ext" rel="stylesheet">
+		<link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="fonts/IcoMoon/icomoon.css">
+		<link rel="stylesheet" href="fonts/linearicon/style.css">
+		<!-- Mobile Menu -->
+		<link type="text/css" rel="stylesheet" href="css/jquery.mmenu.all.css" />
+		
+		<!-- Main Style -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!-- Favicons
+			================================================== -->
+		<link rel="shortcut icon" href="http://placehold.it/32x32/ccc.jpg">
+	</head>
+	<body>
+		
+		<div class="full-screen-warp bg-404"  style="height:150vh">
+			<div class="col-md-6 col-md-offset-3">
+				<div class="warp-inner-404 text-center">
+					<a href="#">
+						<img src="images/arth.png" height="200" width="150" class="img-responsive" alt="Image">
+					</a>
+					<p class="sub-text-404">We are really sorry, but the page you requested is missing..
+					Perhaps searching again can help. Or back to <a href="index.html">home page</a> </p>
+					<div class="box-text-404">
+						<span>ERROR</span>
+						<strong>404</strong>
+						
+						
+					</div>
+					</div>
+			</div>
+		</div>
+
+
+		<!-- Switcher -->
+		<!-- End Switcher -->
+		<!-- SCRIPT -->
+		<script src="js/vendor/jquery.min.js"></script>
+		<script src="js/vendor/bootstrap.js"></script>
+		
+		<!-- PreLoad
+			================================================== --> 
+		
+		<!-- Demo Switcher
+			================================================== -->
+		<!-- <script src="switcher/demo.js"></script> -->
+		
+	</body>
+
+</html>
+
